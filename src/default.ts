14b761568e1f2240c0c0f3b08e8312090a948503
Overbye <Overbye@users.noreply.github.com>
2023-03-14 05:36:14 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,6 +8,6 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 欢迎使用ChatGPT
-- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。
-- 开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。
+- Shift + Enter 换行。
+- 开头输入 / 或者 空格 搜索 Prompt 预设。
 - 点击输入框滚动到底部。`
