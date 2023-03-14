b4a1e7a32ae1f1629c73e3331539fb598bf2f4af
Overbye <Overbye@users.noreply.github.com>
2023-03-14 23:46:57 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -7,8 +7,8 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-欢迎使用 ChatGPT PLUS，本项目由 [OpenAI API](https://platform.openai.com/docs/guides/chat)  驱动。
+欢迎使用 [ChatGPT PLUS](https://chatgptplus.vercel.app/)，本项目由 [OpenAI API](https://platform.openai.com/docs/guides/chat)  驱动。
 - 本站无后台，不保存任何用户数据。
-- 按[[Shift]] + [[Enter]] 进行换行。
-- 开头输入 [[/]] 或者 [[空格]] Prompt 预设。
+- 按[Shift] + [Enter] 进行换行。
+- 开头输入 [/] 或者 [空格] Prompt 预设。
 - 点击输入框滚动到底部。`
